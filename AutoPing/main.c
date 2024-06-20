820a809deccd459399581d2e42e919c3095e92c8
Rayan-AL3SSERI <74124634+0xHailStorm@users.noreply.github.com>
2024-06-20 22:54:21 +0300

    Update and rename main.C to main.c

--- AutoPing/main.c
+++ AutoPing/main.c
@@ -1,10 +1,14 @@
-﻿#include <windows.h>
+#include <windows.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <tlhelp32.h>
+#include <Xinput.h>
 #include <locale.h>
 
+#pragma comment(lib, "Xinput.lib")
+
+BOOL controllerConnected = FALSE;
 // Function to create a default JSON file if it doesn't exist
 void createDefaultJSON(const char* filename) {
     FILE* file;
@@ -14,7 +18,7 @@ void createDefaultJSON(const char* filename) {
         exit(1);
     }
 
-    const char* defaultContent = "{\n    \"ping_button\": \"x\",\n    \"status_button\": \"R\"\n}\n";
+    const char* defaultContent = "{\n    \"ping_button\": \"l\",\n    \"status_button\": \"k\"\n}\n";
     fwrite(defaultContent, sizeof(char), strlen(defaultContent), file);
     fclose(file);
     printf("Default JSON file created.\n");
@@ -63,11 +67,6 @@ void readButtonsFromJSON(const char* filename, char* pingButton, char* statusBut
     free(buffer);
 }
 
-// Source: https://www.cplusplus.com/reference/cstdio/fopen/
-//         https://www.cplusplus.com/reference/cstdio/fseek/
-//         https://www.cplusplus.com/reference/cstdio/ftell/
-//         https://www.cplusplus.com/reference/cstring/strstr/
-
 // Function to send a key using SendInput
 void SendKey(char key) {
     INPUT inputs[2] = { 0 };
@@ -96,9 +95,6 @@ void SendKey(char key) {
     // Send the key up
     SendInput(1, &inputs[1], sizeof(INPUT));
 }
-
-
-// Source: https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendinput
 
 // Structure to hold process information
 typedef struct {
@@ -139,13 +135,46 @@ int isProcessRunning(const wchar_t* processName, ProcessInfo* pInfo) {
     return exists;
 }
 
-// Source: https://docs.microsoft.com/en-us/windows/win32/toolhelp/taking-a-snapshot-and-viewing-processes
+// Function to check if a game controller is connected
+BOOL isControllerConnected() {
+    XINPUT_STATE state;
+    ZeroMemory(&state, sizeof(XINPUT_STATE));
+    DWORD result = XInputGetState(0, &state);
+    return (result == ERROR_SUCCESS);
+}
+
+// Function to get the type of game controller connected
+const char* getControllerType() {
+    XINPUT_CAPABILITIES caps;
+    ZeroMemory(&caps, sizeof(XINPUT_CAPABILITIES));
+    if (XInputGetCapabilities(0, XINPUT_FLAG_GAMEPAD, &caps) == ERROR_SUCCESS) {
+        switch (caps.SubType) {
+        case XINPUT_DEVSUBTYPE_GAMEPAD:
+            return "Xbox Controller";
+        case XINPUT_DEVSUBTYPE_WHEEL:
+            return "Wheel";
+        case XINPUT_DEVSUBTYPE_ARCADE_STICK:
+            return "Arcade Stick";
+        case XINPUT_DEVSUBTYPE_FLIGHT_STICK:
+            return "Flight Stick";
+        case XINPUT_DEVSUBTYPE_DANCE_PAD:
+            return "Dance Pad";
+        case XINPUT_DEVSUBTYPE_GUITAR:
+            return "Guitar";
+        case XINPUT_DEVSUBTYPE_DRUM_KIT:
+            return "Drum Kit";
+        default:
+            return "Unknown";
+        }
+    }
+    return "None";
+}
 
 void setConsoleColors(HANDLE hConsole, WORD color) {
     SetConsoleTextAttribute(hConsole, color);
 }
 
-void printDashboard(HANDLE hConsole, char pingButton, char statusButton, int running, const ProcessInfo* pInfo) {
+void printDashboard(HANDLE hConsole, char pingButton, char statusButton, int running, const ProcessInfo* pInfo, const char* controllerType) {
     system("cls"); // Clear console screen
 
     setConsoleColors(hConsole, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY); // Light blue
@@ -154,7 +183,7 @@ void printDashboard(HANDLE hConsole, char pingButton, char statusButton, int run
     printf("========================================\n");
 
     setConsoleColors(hConsole, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY); // Yellow
-    printf("Developed by: Just#1337 - @0xHailStorm\n");
+    printf("Developed by: Just#1337 - @0xHailStorm\nDiscord : @Win32Apis - https://discord.gg/kom\n");
 
     setConsoleColors(hConsole, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY); // Light blue
     printf("========================================\n");
@@ -197,7 +226,18 @@ void printDashboard(HANDLE hConsole, char pingButton, char statusButton, int run
 
     setConsoleColors(hConsole, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY); // Light blue
     printf("========================================\n");
+    setConsoleColors(hConsole, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY); // Light blue
+    printf("[+] Controller: ");
+    if (controllerType == "None") {
+        setConsoleColors(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY); // Red
+        printf("%s\n", controllerType);
+    }
+    else {
+        setConsoleColors(hConsole, FOREGROUND_GREEN | FOREGROUND_INTENSITY); // Green
+        printf("%s\n", controllerType);
+    }
 
+    setConsoleColors(hConsole, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY); // Light blue
     if (pInfo->pid != 0) {
         printf("[+] Process Name: ");
         setConsoleColors(hConsole, FOREGROUND_RED | FOREGROUND_INTENSITY); // Red
@@ -218,43 +258,61 @@ void printDashboard(HANDLE hConsole, char pingButton, char statusButton, int run
         printf("[-] Target process not running.\n");
     }
 
-    setConsoleColors(hConsole, FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY); // Reset to light blue
+   
 }
 
-// Source for console text color: https://docs.microsoft.com/en-us/windows/console/setconsoletextattribute
+// Function to send key press when R2 or RT is pressed on controller
+void checkControllerAndSendKey(char pingButton) {
+    XINPUT_STATE state;
+    ZeroMemory(&state, sizeof(XINPUT_STATE));
+
+    if (XInputGetState(0, &state) == ERROR_SUCCESS) {
+        if (state.Gamepad.bRightTrigger > XINPUT_GAMEPAD_TRIGGER_THRESHOLD) {
+            SendKey(pingButton);
+            Sleep(3);
+            SendKey(pingButton);
+        }
+    }
+}
 
 void SetConsoleWindowSize(int width, int height) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-
     COORD bufferSize = { width, height };
     SetConsoleScreenBufferSize(hConsole, bufferSize);
 
-    
     SMALL_RECT windowSize = { 0, 0, width - 1, height - 1 };
     SetConsoleWindowInfo(hConsole, TRUE, &windowSize);
 }
-/* Source:
-    https://learn.microsoft.com/en-us/windows/console/setconsolescreenbuffersize
-    https://learn.microsoft.com/en-us/windows/console/setconsolewindowinfo
-*/
 
 int main() {
-    SetConsoleWindowSize(65, 18);
+    SetConsoleWindowSize(65, 20);
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
     char pingButton, statusButton;
     readButtonsFromJSON("config.json", &pingButton, &statusButton);
 
+
     int running = 0;
     ProcessInfo pInfo = { 0 };
     int processWasRunning = 0;
-    printDashboard(hConsole, pingButton, statusButton, running, &pInfo);
+
+    const char* controllerType = isControllerConnected() ? getControllerType() : "None";
+    printDashboard(hConsole, pingButton, statusButton, running, &pInfo, controllerType);
+
     while (1) {
         int processIsRunning = isProcessRunning(L"cod.exe", &pInfo);
 
+        BOOL currentControllerState = isControllerConnected();
+        if (currentControllerState != controllerConnected) {
+            controllerConnected = currentControllerState;
+            controllerType = controllerConnected ? getControllerType() : "None";
+            printDashboard(hConsole, pingButton, statusButton, running, &pInfo, controllerType);
+        }
+
         if (processIsRunning && !processWasRunning) {
-            printDashboard(hConsole, pingButton, statusButton, running, &pInfo);
+            controllerType = controllerConnected ? getControllerType() : "None";
+            printDashboard(hConsole, pingButton, statusButton, running, &pInfo, controllerType);
         }
 
         if (processIsRunning) {
@@ -263,24 +321,27 @@ int main() {
                 Sleep(10); // Add a short delay to prevent rapid firing
                 SendKey(pingButton);
             }
-
-            if (GetAsyncKeyState(VkKeyScanA(statusButton)) & 0x8000) {
+            if ((GetKeyState(VkKeyScanA(statusButton)) & 0x8000) != 0) {
                 running = !running;
-                printDashboard(hConsole, pingButton, statusButton, running, &pInfo);
+                controllerType = controllerConnected ? getControllerType() : "None";
+                printDashboard(hConsole, pingButton, statusButton, running, &pInfo, controllerType);
                 if (!running) {
-                    printf("[-] Program paused...\n");
                 }
                 else {
                     running = 1;
                     readButtonsFromJSON("config.json", &pingButton, &statusButton); // Reload settings
-                    printf("[+] Program resumed with new settings...\n");
                 }
                 Sleep(300);
+            }
+
+            if (running) {
+                checkControllerAndSendKey(pingButton);
             }
         }
         else {
             if (processWasRunning) {
-                printDashboard(hConsole, pingButton, statusButton, running, &pInfo);
+                controllerType = controllerConnected ? getControllerType() : "None";
+                printDashboard(hConsole, pingButton, statusButton, running, &pInfo, controllerType);
             }
         }
 
@@ -292,26 +353,38 @@ int main() {
 }
 
 
-// Source for reading JSON: https://www.cplusplus.com/reference/cstdio/fopen/
-//                          https://www.cplusplus.com/reference/cstdio/fseek/
-//                          https://www.cplusplus.com/reference/cstdio/ftell/
-//                          https://www.cplusplus.com/reference/cstring/strstr/
+// Source for checking if a controller is connected: 
+// https://docs.microsoft.com/en-us/windows/win32/api/xinput/nf-xinput-xinputgetstate
 
+// Source for getting the type of the connected controller: 
+// https://docs.microsoft.com/en-us/windows/win32/api/xinput/nf-xinput-xinputgetcapabilities
 
+// Source for setting console text colors: 
+// https://docs.microsoft.com/en-us/windows/console/setconsoletextattribute
 
-// Source for creating default JSON: https://www.cplusplus.com/reference/cstdio/fopen/
-//                                   https://www.cplusplus.com/reference/cstdio/fwrite/
+// Source for printing the dashboard: 
+// https://docs.microsoft.com/en-us/windows/console/cls
 
+// Source for sending a key using SendInput: 
+// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendinput
 
+// Source for checking if a specific process is running and gathering its information: 
+// https://docs.microsoft.com/en-us/windows/win32/toolhelp/taking-a-snapshot-and-viewing-processes
 
-// Source for SendInput: https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-sendinput
+// Source for checking if R2 or RT button is pressed on the controller: 
+// https://docs.microsoft.com/en-us/windows/win32/api/xinput/nf-xinput-xinputgetstate
 
+// Source for setting console window size: 
+// https://learn.microsoft.com/en-us/windows/console/setconsolescreenbuffersize
+// https://learn.microsoft.com/en-us/windows/console/setconsolewindowinfo
 
-// Source for checking process: https://docs.microsoft.com/en-us/windows/win32/toolhelp/taking-a-snapshot-and-viewing-processes
+// Source for reading JSON: 
+// https://www.cplusplus.com/reference/cstdio/fopen/
+// https://www.cplusplus.com/reference/cstdio/fseek/
+// https://www.cplusplus.com/reference/cstdio/ftell/
+// https://www.cplusplus.com/reference/cstring/strstr/
 
+// Source for creating default JSON: 
+// https://www.cplusplus.com/reference/cstdio/fopen/
+// https://www.cplusplus.com/reference/cstdio/fwrite/
 
-// Source for console text color: https://docs.microsoft.com/en-us/windows/console/setconsoletextattribute
-
-
-// Source for console WindowSize: https://learn.microsoft.com/en-us/windows/console/setconsolescreenbuffersize
-//                                https://learn.microsoft.com/en-us/windows/console/setconsolewindowinfo
